152f24cc23b62c399a8b7822689275aedddf6461
fushan <957050566@qq.com>
2017-12-25 16:56:35 +0800

    20171225
    reason：修改输出内容

--- kscc-api/src/main/java/com/fable/kscc/api/utils/MyThread.java
+++ kscc-api/src/main/java/com/fable/kscc/api/utils/MyThread.java
@@ -7,5 +7,6 @@ public class MyThread extends Thread {
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName()+"正在执行。。。");
+        System.out.println("修改某个文件上传到github");
     }
 }
